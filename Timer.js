5bf657a0e01ca0c3c72bba454bd0525997db4b2e
Bhavna Pereira <pereib4@mcmaster.ca>
2021-03-06 22:27:03 -0500

    error fix

--- Timer.js
+++ Timer.js
@@ -4,43 +4,6 @@
 
   });
 
-  // var hours = 0;
-  // var minutes = 0;
-  // var seconds = 0;
-  // var interval = null;
-
-  // document.getElementById('Hours').addEventListener('change', e => {
-  //     hours = +e.target.value;
-  // });
-
-  // document.getElementById('Minutes').addEventListener('change', e => {
-  //     minutes = +e.target.value;
-  // });
-
-  // document.getElementById('Seconds').addEventListener('change', e => {
-  //     seconds = +e.target.value;
-  // });
-
-  // document.getElementById('submit').addEventListener('click', () => {
-  //     var timeInSeconds = (hours * 60 * 60) +
-  //         (minutes * 60) +
-  //         seconds;
-
-  //     var displayTime = () => {
-  //         var displayHours = Math.floor(timeInSeconds / (60 * 60));
-  //         var remainder = timeInSeconds - (displayHours * 60 * 60);
-  //         var displayMinutes = Math.floor(remainder / 60);
-  //         var displaySeconds = remainder - (displayMinutes * 60);
-  //         document.getElementById("Timer").innerHTML = displayHours + " : " + displayMinutes + " : " + displaySeconds;
-  //     };
-  //     interval = setInterval(() => {
-  //         displayTime();
-  //         timeInSeconds -= 1;
-  //         if (timeInSeconds < 0) {
-  //             clearInterval(interval);
-  //         }
-  //     }, 1000);
-  // });
   var mySeconds;
   var intervalHandle;
 
@@ -71,7 +34,7 @@
     
   }
   function startCounter(){
-    var myInput=document.getElementById("minutes").value;
+    var myInput=document.getElementById("Timer").value;
     if (isNaN(myInput)){
       alert("Type a valid number please");
       return;
@@ -80,7 +43,7 @@
     
     intervalHandle=setInterval(tick, 1000);
     
-    document.getElementById("inputArea").style.display="Your break is up!";
+    document.getElementById("inputArea").style.display="none";
     
     
   }
