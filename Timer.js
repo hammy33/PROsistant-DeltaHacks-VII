211da61cb8e043c9ecd7759c8cd5aa5655804f4e
Bhavna Pereira <pereib4@mcmaster.ca>
2021-03-06 22:58:36 -0500

    please please please work

--- Timer.js
+++ Timer.js
@@ -3,70 +3,63 @@
      var activeTabId = activeTab.url; 
 
   });
-
   var mySeconds;
   var intervalHandle;
 
   function resetPage(){
     document.getElementById("Timer").style.display="none";  
-    
-    
+
+
   }
   function tick(){
-    var timeDisplay=document.getElementById("Timer");
-    
+    var timeDisplay=document.getElementById("time");
+
     var min=Math.floor(mySeconds/60);
     var sec=mySeconds-(min*60);
-    
+
     if (sec < 10) {
       sec="0"+sec;
     }
-    
+
     var message=min.toString()+":"+sec;
-    
+
     timeDisplay.innerHTML=message;
-    
+
     if(mySeconds===0){
-      alert("Done");
-      clearInterval(intervalHandle);
-      resetPage();
+      chrome.tabs.update(activeTabId)
     }
     mySeconds--;
-    
-    
+
+
   }
   function startCounter(){
-    var myInput=document.getElementById("Timer").value;
+    var myInput=document.getElementById("minutes").value;
     if (isNaN(myInput)){
       alert("Type a valid number please");
       return;
     }
     mySeconds=myInput*60;
-    
+
     intervalHandle=setInterval(tick, 1000);
-    
-    document.getElementById("Timer").style.display="none";
-    
-    
+
+    document.getElementById("inputArea").style.display="none";
   }
-
-
   window.onload=function(){
-    var myInput=document.createElement("Time");
+    var myInput=document.createElement("input");
     myInput.setAttribute("type","text");
     myInput.setAttribute("id","minutes");
-    
-    var myButton=document.createElement("submit");
+
+    var myButton=document.createElement("input");
     myButton.setAttribute("type","button");
     myButton.setAttribute("value","submit");
-    
+
     myButton.onclick=function(){
       startCounter(); 
-      
+
     }
     document.getElementById("Timer").appendChild(myInput);
-    document.getElementById("submit").appendChild(myButton);
-    
-    
+    document.getElementById("Timer").appendChild(myButton);
+
+
   }
 
